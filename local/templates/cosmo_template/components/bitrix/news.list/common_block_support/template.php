2bc04b1245dc9af884690a896abbeb0463b3873c
Alex <gudtxqv370@1secmail.ru>
2024-09-12 09:59:08 +0300

    поправил верстку

--- local/templates/cosmo_template/components/bitrix/news.list/common_block_support/template.php
+++ local/templates/cosmo_template/components/bitrix/news.list/common_block_support/template.php
@@ -12,20 +12,20 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<!--    <div class="support container">-->
-<div class="section-banner container">
-    <div class="section-banner-wrapper">
-        <? if ($arResult["ITEMS"][0]["ID"] == "57"): ?>
-            <video playsinline muted autoplay loop
-                   poster="<?= $arResult["ITEMS"][0]["PROPERTIES"]["PICTURE"]["VALUE"] ?>"
-                   src="<?= $arResult["ITEMS"][0]["PROPERTIES"]["VIDEO"]["VALUE"] ?>"></video>
-            <div class=" section-banner-block">
-                <p class="section-banner-name"><?= $arResult["ITEMS"][0]["PROPERTIES"]["HEADING"]["VALUE"] ?></p>
-                <h2 class="section-banner-title"><?= $arResult["ITEMS"][0]["PROPERTIES"]["SECTION"]["VALUE"] ?></h2>
-                <div class="section-banner-text">
-                    <?= $arResult["ITEMS"][0]["PROPERTIES"]["DESCRIPTION"]["VALUE"] ?>
+<div class="support container">
+    <div class="section-banner container">
+        <div class="section-banner-wrapper">
+            <? if ($arResult["ITEMS"][0]["ID"] == "57"): ?>
+                <video playsinline muted autoplay loop
+                       poster="<?= $arResult["ITEMS"][0]["PROPERTIES"]["PICTURE"]["VALUE"] ?>"
+                       src="<?= $arResult["ITEMS"][0]["PROPERTIES"]["VIDEO"]["VALUE"] ?>"></video>
+                <div class=" section-banner-block">
+                    <p class="section-banner-name"><?= $arResult["ITEMS"][0]["PROPERTIES"]["HEADING"]["VALUE"] ?></p>
+                    <h2 class="section-banner-title"><?= $arResult["ITEMS"][0]["PROPERTIES"]["SECTION"]["VALUE"] ?></h2>
+                    <div class="section-banner-text">
+                        <?= $arResult["ITEMS"][0]["PROPERTIES"]["DESCRIPTION"]["VALUE"] ?>
+                    </div>
                 </div>
-            </div>
-        <? endif; ?>
+            <? endif; ?>
+        </div>
     </div>
-</div>
